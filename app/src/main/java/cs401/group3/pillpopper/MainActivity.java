c4516ac299bc97fb09524e36c8f1b9baa6a3dd77
Yucheng Zheng <yuchengmojian@gmail.com>
2020-04-16 08:53:04 -0700

    just something

--- app/src/main/java/cs401/group3/pillpopper/MainActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    public String patient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_start);
     }
 }
