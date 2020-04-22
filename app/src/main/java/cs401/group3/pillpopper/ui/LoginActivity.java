08ef1ebd7b301e00641644c64578caedc51a141d
Lauren Dennedy <laurendennedy@gmail.com>
2020-04-22 10:15:37 -0700

    Safe Delete MainActivity and XML Layout

--- app/src/main/java/cs401/group3/pillpopper/ui/LoginActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/LoginActivity.java
@@ -1,4 +1,33 @@
 package cs401.group3.pillpopper.ui;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cs401.group3.pillpopper.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button mRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_start);
+
+        mRegisterButton = findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchRegisterActivity();
+            }
+        });
+    }
+
+    private void launchRegisterActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/cs401/group3/pillpopper/ui/RegisterActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/RegisterActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import cs401.group3.pillpopper.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
