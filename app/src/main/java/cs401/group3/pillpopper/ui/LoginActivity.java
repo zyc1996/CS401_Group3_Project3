41a4fd0ccb2d03f92b0d87621f6e76e6f7c5841c
Lauren Dennedy <laurendennedy@gmail.com>
2020-04-22 10:35:12 -0700

    some activity navigation complete

--- app/src/main/java/cs401/group3/pillpopper/ui/LoginActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/LoginActivity.java
@@ -11,23 +11,37 @@ import cs401.group3.pillpopper.R;
 
 public class LoginActivity extends AppCompatActivity {
     private Button mRegisterButton;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_start);
 
-        mRegisterButton = findViewById(R.id.register_button);
+        mRegisterButton = findViewById(R.id.registerButton);
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchRegisterActivity();
             }
         });
+
+        mLoginButton = findViewById(R.id.signInButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchHomePageActivity();
+            }
+        });
     }
 
     private void launchRegisterActivity() {
         Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchHomePageActivity() {
+        Intent intent = new Intent(this, PatientHomepageActivity.class);
         startActivity(intent);
     }
 }
