5215263f0c8f3eece1138a1cf99f695ab45e71b6
Lauren Dennedy <laurendennedy@gmail.com>
2020-04-23 08:33:04 -0700

    Update RegisterActivity.java

--- app/src/main/java/cs401/group3/pillpopper/ui/RegisterActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/RegisterActivity.java
@@ -39,70 +39,79 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         // Get the current login instance from the Firebase Authenticator
         mAuth = FirebaseAuth.getInstance();
 
-
-        //grab object poitning to BUtton in GUI and register THIS class as its event handler
+        // Grab object pointing to button in GUI and register THIS class as its event handler
         ((Button) findViewById(R.id.registerButton)).setOnClickListener(this);
     }
-
-
 
     // When the page starts
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        //FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
     }
 
     /**
      * Event listener to perform firebase registration of new user using an email and password
      * @param v View from the activity to pass to onClick
      */
+
     @Override
     public void onClick(View v) {
-        //STEP 1: get user info from GUI
-        // Grab username, password, confirm password, doctor code the user typed in
-        username = ((EditText) findViewById(R.id.userNameEdit)).getText().toString();
 
+        // STEP 1: get user info from GUI
+        // Grab username, password, confirm password, doctor code the user typed in
         // Note: Need to rename XML Ids
+
+        username = ((EditText) findViewById(R.id.userNameEdit)).getText().toString();
         password = ((EditText) findViewById(R.id.editPassword)).getText().toString();
         confirmPassword = ((EditText) findViewById(R.id.confirmPassword)).getText().toString();
         doctorCode = ((EditText) findViewById(R.id.doctorCode)).getText().toString();
 
+        // STEP2: Data validation
+        // Confirm that all 3 fields have data etc (username, password, confirm password)
+        // confirm password and confirm password are the same before continuing
 
+        if (username == null) {
+            Toast.makeText(RegisterActivity.this, "Please enter a username.",
+                    Toast.LENGTH_SHORT).show();
 
-        //STEP2:  data vailadtion
-        // confirm that all 3 fields have data etc (username, apssword, confirmpassword)
-        // confirm password and confrimPasword are the asme before continuing
+        } else if (password == null) {
+            Toast.makeText(RegisterActivity.this, "Please enter a password.",
+                    Toast.LENGTH_SHORT).show();
 
+        } else if (confirmPassword == null) {
+            Toast.makeText(RegisterActivity.this, "Please confirm your password.",
+                    Toast.LENGTH_SHORT).show();
 
+        } else if (!password.equals(confirmPassword)) {
+            Toast.makeText(RegisterActivity.this, "Password and confirm password do not match.",
+                    Toast.LENGTH_SHORT).show();
 
+        } else {
 
-        //STEP 3: at this point all info correct go to create new user
-        mAuth.createUserWithEmailAndPassword(username, password)
+        // STEP 3: At this point all info correct go to create new user
+            mAuth.createUserWithEmailAndPassword(username, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
+
                             Log.d("REGISTER", "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Log.d("REGISTER", "createUserWithEmail:success");
                             Toast.makeText(RegisterActivity.this, "Registration Successful.",
                                     Toast.LENGTH_LONG).show();
-                            //updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
+
                             Log.d("REGISTER", "createUserWithEmail:failure", task.getException());
                             Toast.makeText(RegisterActivity.this, "Registration failed.",
                                     Toast.LENGTH_LONG).show();
-                            //updateUI(null);
                         }
-
-                        // ...
                     }
                 });
+
+        }
+
     }
 
 }
