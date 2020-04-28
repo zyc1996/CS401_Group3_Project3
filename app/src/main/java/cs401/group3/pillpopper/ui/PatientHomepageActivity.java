2343cecee29459fe5f9f51fe62159dfc77df20b5
Zyc-K Death <yuchengmojian@gmail.com>
2020-04-28 09:17:19 -0700

    Patient Profile Edit Activity Will launch

--- app/src/main/java/cs401/group3/pillpopper/ui/PatientHomepageActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/PatientHomepageActivity.java
@@ -1,6 +1,8 @@
 package cs401.group3.pillpopper.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,10 @@ public class PatientHomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_patient);
+    }
+
+    public void launchPatientProfileEdit(View view) {
+        Intent intent  = new Intent(this,EditPatientProfileActivity.class);
+        startActivity(intent);
     }
 }
