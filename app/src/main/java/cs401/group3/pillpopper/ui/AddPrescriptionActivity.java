01b2b4179f5bddd5d1e1e735abe6e1841a85142a
Zyc-K <yuchengmojian@gmail.com>
2020-04-29 20:58:20 -0700

    added add prescription

---
+++ app/src/main/java/cs401/group3/pillpopper/ui/AddPrescriptionActivity.java
@@ -0,0 +1,14 @@
+package cs401.group3.pillpopper.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddPrescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_add_prescription);
+    }
+}
--- app/src/main/java/cs401/group3/pillpopper/ui/PatientProfileActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/PatientProfileActivity.java
@@ -44,6 +44,8 @@ public class PatientProfileActivity extends AppCompatActivity {
         Intent intent  = new Intent(this, ProfileEditActivity.class);
         String patient_Name = mName.getText().toString();
         String patient_description = mDescription.getText().toString();
+
+        //pass the name and description over, no picture URL yet
         intent.putExtra("name",patient_Name);
         intent.putExtra("description",patient_description);
         startActivityForResult(intent,REQUEST_CODE);
@@ -54,10 +56,12 @@ public class PatientProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+            //updates picture URL
             if(data.hasExtra("picture_URL")){
                 Log.d("TagP","Picture URL returned");
                 //TODO:Picture stuff later
             }
+            //update personal description
             if(data.hasExtra("description")){
                 Log.d("TagD","Description returned");
                 String description = data.getExtras().getString("description");
