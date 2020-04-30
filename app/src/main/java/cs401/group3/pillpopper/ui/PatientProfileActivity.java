b579df81c5cd147c90fc3f1cb8d8f0dcb3d38d77
Lauren Dennedy <laurendennedy@gmail.com>
2020-04-29 21:31:38 -0700

    Toolbars for Profile

--- app/src/main/java/cs401/group3/pillpopper/ui/PatientProfileActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/PatientProfileActivity.java
@@ -3,11 +3,14 @@ package cs401.group3.pillpopper.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import cs401.group3.pillpopper.R;
 
@@ -23,11 +26,29 @@ public class PatientProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_patient);
         mDescription = findViewById(R.id.description_text);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.patientProfileToolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        setSupportActionBar(toolbar);
     }
 
-    public void launchProfileEdit(View view) {
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    public void profileBack(MenuItem back) {
+        Intent intent = new Intent(this, HomepagePatientActivity.class);
+        startActivity(intent);
+    }
+
+    public void editProfile(MenuItem edit) {
         Intent intent  = new Intent(this, ProfileEditActivity.class);
-        startActivityForResult(intent,REQUEST_CODE);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
     //receiving changes
