a6c6d0b8ff90fe3c926621cdbda10f5988dc19f3
Zyc-K Death <yuchengmojian@gmail.com>
2020-04-29 17:09:01 -0700

    edit activity updated

--- app/src/main/java/cs401/group3/pillpopper/ui/DoctorProfileActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/DoctorProfileActivity.java
@@ -2,7 +2,9 @@ package cs401.group3.pillpopper.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,13 @@ import cs401.group3.pillpopper.R;
 public class DoctorProfileActivity extends AppCompatActivity {
 
     private int REQUEST_CODE = 1;
+    private TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_doctor);
+        mDescription = findViewById(R.id.description_text);
     }
 
     public void launchProfileEdit(View view) {
@@ -29,7 +33,16 @@ public class DoctorProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK && requestCode == REQUEST_CODE){
-
+            assert data != null;
+            if(data.hasExtra("picture_URL")){
+                Log.d("TagP","Picture URL returned");
+                //TODO:Picture stuff later
+            }
+            if(data.hasExtra("description")){
+                Log.d("TagD","Description returned");
+                String description = data.getExtras().getString("description");
+                mDescription.setText(description);
+            }
         }
     }
 }
