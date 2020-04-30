850eeff1b98788ec3a068d95f2f57fb6bed6a38b
JGilcreast <varxe7@gmail.com>
2020-04-29 19:33:16 -0700

    Java data class implementation, commit 1

--- app/src/main/java/cs401/group3/pillpopper/data/Doctor.java
+++ app/src/main/java/cs401/group3/pillpopper/data/Doctor.java
@@ -4,4 +4,48 @@ import java.util.ArrayList;
 
 public class Doctor extends User {
 
+    private int doctor_id;
+     private ArrayList<Integer> patient_id;
+     //private ArrayList<String> patient_name;
+     //private ArrayList<String> patient_pic;
+
+     public Doctor(){
+         patient_id = new ArrayList<Integer>();
+         //patient_name = new ArrayList<String>();
+         //patient_pic = new ArrayList<String>();
+    }
+
+    public int get_doctor_id() {
+        return doctor_id;
+    }
+
+    public void set_doctor_id(int doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    public boolean add_patient(int patient_id_in){
+         //check in database if patient exists
+        //if exists
+        /*patient_name_in & patient_pic_in from database
+        //this.patient_id.add(patient_id_in);
+        //this.patient_name.add(patient_name_in); //get from database
+        //this.patient_pic.add(patient_pic_in); //get from database
+        */
+        return true;
+
+        //if does not exist
+        //return false;
+    }
+
+    public boolean remove_patient(int patient_id_in){
+         for(int i = 0; i < this.patient_id.size(); i++){
+             if (patient_id_in == this.patient_id.get(i)){
+                 this.patient_id.remove(i);
+                 //this.patient_name.remove(i);
+                 //this.patient_pic.remove(i);
+                 return true;
+             }
+         }
+         return false;
+    }
 }
--- app/src/main/java/cs401/group3/pillpopper/data/Message.java
+++ app/src/main/java/cs401/group3/pillpopper/data/Message.java
@@ -6,42 +6,59 @@ public class Message {
     private int id;
     private String content;
     private boolean sent_by_user, edited;
-    private Date timestamp;
+    private Date created_at;
 
     public Message(String input, boolean from_user, int id_in) {
-        timestamp.getTime();
+        created_at.getTime();
         content = input;
         id = id_in;
         sent_by_user = from_user; //is the message from the user? If not, it is from the other user
         edited = false;
+        created_at = new Date();
+        created_at.getTime();
     }
 
     public Message() {
-        this.timestamp.getTime();
+        this.created_at.getTime();
         this.content = "";
         sent_by_user = false;
         edited = false;
         id = -1; //value to mark non-normal message
+        created_at = new Date();
+        created_at.getTime();
     }
 
-    public String get_content() { return content; }
-
-    public int get_id() { return id; }
-
-    public void content_edit(String input) {
-        edited = true;
-        content = input;
+    public int get_id() {
+        return id;
     }
 
-    public boolean from_this_user(){
+    public void set_id(int id) {
+        this.id = id;
+    }
+
+    public String get_content() {
+        return content;
+    }
+
+    public void set_content(String content) {
+        this.edited = true;
+        this.content = content;
+    }
+
+    public boolean is_sent_by_user() {
         return sent_by_user;
     }
 
-    public boolean is_edited(){
+    public void set_sent_by_user(boolean sent_by_user) { //false means this message was
+                                                        // not from the user containing this object
+        this.sent_by_user = sent_by_user;
+    }
+
+    public boolean is_edited() {
         return edited;
     }
 
-    public Date get_timestamp() {
-        return timestamp;
+    public Date get_created_at() {
+        return created_at;
     }
 }
