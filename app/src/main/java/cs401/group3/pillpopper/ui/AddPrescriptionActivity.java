9f5d3f33d63194d47cb24536f08d8b9d292d338c
Zyc-K <yuchengmojian@gmail.com>
2020-04-29 21:23:56 -0700

    add prescription xml complete

--- app/src/main/java/cs401/group3/pillpopper/ui/AddPrescriptionActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/AddPrescriptionActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import cs401.group3.pillpopper.R;
+
 public class AddPrescriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_add_prescription);
+        setContentView(R.layout.activity_add_prescription);
     }
 
 
