3e279e510be0c398a14947d53935674c9decfbcf
Lauren Dennedy <laurendennedy@gmail.com>
2020-04-30 19:48:16 -0700

    Recreating Toolbars

--- app/src/main/java/cs401/group3/pillpopper/ui/HomepagePatientActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/HomepagePatientActivity.java
@@ -2,11 +2,14 @@ package cs401.group3.pillpopper.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import cs401.group3.pillpopper.R;
 import cs401.group3.pillpopper.data.Patient;
@@ -23,16 +26,40 @@ public class HomepagePatientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_patient);
 
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.registerToolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        setSupportActionBar(toolbar);
+
         mUserName = findViewById(R.id.user_name_title);
         mUserName.setText(patient.get_user_name());
     }
 
-    public void launchPatientProfile(View view){
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_homepage, menu);
+        return true;
+    }
+
+    public void launchMessages(MenuItem messages) {
+        Intent intent = new Intent(this, MessagesActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchProfile(MenuItem profile) {
         Intent intent = new Intent(this,PatientProfileActivity.class);
         String patientID = patient.get_patient_id();
         intent.putExtra("patient_ID",patientID);
         startActivity(intent);
     }
+
+    public void onLogout(MenuItem logout) {
+        Intent intent = new Intent(this, LoginStartActivity.class);
+        startActivity(intent);
+    }
+
 
     public void launchAddPrescription(View view){
         Intent intent = new Intent (this,AddPrescriptionActivity.class);
@@ -44,6 +71,5 @@ public class HomepagePatientActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
     }
 }
--- app/src/main/java/cs401/group3/pillpopper/ui/LoginRegisterActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/LoginRegisterActivity.java
@@ -1,8 +1,11 @@
 package cs401.group3.pillpopper.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -38,6 +42,11 @@ public class LoginRegisterActivity extends AppCompatActivity implements View.OnC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
 
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.registerToolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        setSupportActionBar(toolbar);
+
         // Get the current login instance from the Firebase Authenticator
         mAuth = FirebaseAuth.getInstance();
 
@@ -49,6 +58,19 @@ public class LoginRegisterActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_back, menu);
+        return true;
+    }
+
+    public void onBack(MenuItem back) {
+        Intent intent = new Intent(this, LoginStartActivity.class);
+        startActivity(intent);
     }
 
     /**
