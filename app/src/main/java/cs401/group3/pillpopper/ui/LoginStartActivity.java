395c12910c1a1506929ad9bbccff4e34a684e18d
Zyc-K <yuchengmojian@gmail.com>
2020-04-30 20:08:21 -0700

    login differs doctor and patient and directs to different home page

--- app/src/main/java/cs401/group3/pillpopper/ui/LoginStartActivity.java
+++ app/src/main/java/cs401/group3/pillpopper/ui/LoginStartActivity.java
@@ -8,14 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import cs401.group3.pillpopper.R;
 
@@ -24,6 +25,7 @@ public class LoginStartActivity extends AppCompatActivity implements View.OnClic
 
     // Firebase authenticator
     private FirebaseAuth mAuth;
+    private ValueEventListener listener;
 
     // Buttons
     private Button mRegisterButton;
@@ -77,7 +79,52 @@ public class LoginStartActivity extends AppCompatActivity implements View.OnClic
                     Toast.LENGTH_SHORT).show();
 
         } else {
+
+            DatabaseReference result = FirebaseDatabase.getInstance().getReference("patients");
+            Query q = result.orderByChild("email").equalTo(username);
+
+            q.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        for (DataSnapshot snapElement : dataSnapshot.getChildren()) {
+                            if (snapElement.child("password").getValue().toString().equals(password)) {
+                                Log.i("my tag", "Logged in successfully");
+                                launchPatientHomePageActivity();
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.i("my tag", "Login error");
+                }
+            });
+
+            result = FirebaseDatabase.getInstance().getReference("doctors");
+            q = result.orderByChild("email").equalTo(username);
+            q.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        for (DataSnapshot snapElement : dataSnapshot.getChildren()) {
+                            if (snapElement.child("password").getValue().toString().equals(password)) {
+                                Log.i("my tag", "Logged in successfully");
+                                launchDoctorHomePageActivity();
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.i("my tag", "Login error");
+                }
+            });
+
             // Step 3: Confirm sign in with mAuth
+/*
             mAuth.signInWithEmailAndPassword(username, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -100,7 +147,7 @@ public class LoginStartActivity extends AppCompatActivity implements View.OnClic
                         }
 
                     });
-
+*/
         }
 
     }
@@ -112,8 +159,12 @@ public class LoginStartActivity extends AppCompatActivity implements View.OnClic
     }
 
     // Private helper method to launch the home page
-    private void launchHomePageActivity() {
+    private void launchPatientHomePageActivity() {
         Intent intent = new Intent(this, HomepagePatientActivity.class);
+        startActivity(intent);
+    }
+    private void launchDoctorHomePageActivity() {
+        Intent intent = new Intent(this, HomepageDoctorActivity.class);
         startActivity(intent);
     }
 
