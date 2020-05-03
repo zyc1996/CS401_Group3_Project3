25fc552afb792b62db0058817c7dbfb5d9cd4b51
Zyc-K <yuchengmojian@gmail.com>
2020-05-02 18:17:39 -0700

    adapter java setup finished

--- app/src/main/java/cs401/group3/pillpopper/adapter/patientAdapter.java
+++ app/src/main/java/cs401/group3/pillpopper/adapter/patientAdapter.java
@@ -1,16 +1,63 @@
 package cs401.group3.pillpopper.adapter;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import cs401.group3.pillpopper.R;
+import cs401.group3.pillpopper.data.Patient;
 
-public class patientAdapter extends AppCompatActivity {
+public class patientAdapter extends RecyclerView.Adapter<patientAdapter.ViewHolder> {
+    private List<Patient> patient;
+    private Context context;
+
+    public patientAdapter(List<Patient> patient, Context context) {
+        this.patient = patient;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_patient,parent,false);
+        return new ViewHolder(view);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_patient);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Patient onePatient = patient.get(position);
+        int tempImage = R.drawable.default_profile;
+
+        holder.mPatientProfile.setImageResource(tempImage);
+        holder.mPatientName.setText(onePatient.get_user_name());
+        holder.mPatientDescription.setText(onePatient.get_personal_description());
     }
+
+    @Override
+    public int getItemCount() {
+        return patient.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView mPatientProfile;
+        public TextView mPatientName;
+        public TextView mPatientDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mPatientProfile = (ImageView) itemView.findViewById(R.id.patient_profile);
+            mPatientName = (TextView) itemView.findViewById(R.id.patient_name);
+            mPatientDescription = (TextView) itemView.findViewById(R.id.patient_description);
+        }
+    }
+
 }
--- app/src/main/java/cs401/group3/pillpopper/adapter/prescriptionAdapter.java
+++ app/src/main/java/cs401/group3/pillpopper/adapter/prescriptionAdapter.java
@@ -1,16 +1,58 @@
 package cs401.group3.pillpopper.adapter;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import cs401.group3.pillpopper.R;
+import cs401.group3.pillpopper.data.Prescription;
 
-public class prescriptionAdapter extends AppCompatActivity {
+public class prescriptionAdapter extends RecyclerView.Adapter<prescriptionAdapter.ViewHolder> {
+
+    private List<Prescription> prescription;
+    private Context context;
+
+    public prescriptionAdapter(List<Prescription> prescription) {
+        this.prescription = prescription;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_prescription,parent,false);
+        return new ViewHolder(view);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_prescription);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Prescription onePrescription = prescription.get(position);
+
+        holder.mPrescriptionStartTime.setText(onePrescription.getStart_time());
+        holder.mPrescriptionDescription.setText(onePrescription.get_content());
+    }
+
+    @Override
+    public int getItemCount() {
+        return prescription.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView mPrescriptionStartTime;
+        public TextView mPrescriptionDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mPrescriptionStartTime = (TextView) itemView.findViewById(R.id.prescription_start_time);
+            mPrescriptionDescription = (TextView) itemView.findViewById(R.id.prescription_description);
+        }
     }
 }
